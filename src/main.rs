ea900460c02bd972396603ffa18d75432f07d3a7
chen_null <abc82766@gmail.com>
2022-02-15 11:14:03 +0800

    Add graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use axum::{
 };
 use lazy_static::lazy_static;
 use sqlx::{migrate, SqlitePool};
+use tokio::signal;
 use tower_http::{
     compression::CompressionLayer,
     cors::{any, CorsLayer},
@@ -49,6 +50,33 @@ pub async fn migrate(db_url: &str) {
             .unwrap();
         migrate!().run(&pool).await.unwrap();
     }
+}
+
+/// Shutdown signal handler, stop the loop
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
 
 #[tokio::main]
@@ -98,6 +126,7 @@ async fn main() {
         .unwrap();
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
 }
